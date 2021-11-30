db3edd94a176540f952546fceb3a279c6cf9ef1e
gabrielanavas <navasgabriela17@gmail.com>
2021-11-29 21:04:34 -0600

    practice

--- chapter2.py
+++ chapter2.py
@@ -1,37 +1,53 @@
-#Exercise 2
-name = input("What is your name?")
-print("Hello", name)
+while True:
+    print("\n\n PAGINA PRINCIPAL \n1. Name \n2. Gross Pay\n3. Type \n4. Celsius \n5.salir Seleccione una opcion")
+    opcion = input(">> ")
 
-#Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay
-hour =input("Enter Hours:")
-rate =input("Enter rate:")
+    if opcion == "1":
+    #Exercise 1
+        name = input("What is your name?")
+        print("Hello", name)
+    elif opcion == "2":  
+    #Exercise 2: Write a program to prompt the user for hours and rate per hour to compute gross pay
+        hour =input("Enter Hours:")
+        rate =input("Enter rate:")
 
-hours= float(hour)
-rates=float(rate)
+        hours= float(hour)
+        rates=float(rate)
 
-pay = (hours*rates)
-print(pay)
+        pay = (hours*rates)
+        print(pay)
 
-#Exercise 4 write the value and type of the expression 
-width =17
-height = 12.0
+    elif opcion == "3":
+    #Exercise 3 write the value and type of the expression 
+        width =17
+        height = 12.0
 
-test1= (width//2)
-print(test1)
+        test1= (width//2)
+        print(test1)
 
-test2 =(width/2.0)
-print(test2)
+        test2 =(width/2.0)
+        print(test2)
 
-test3= (height/3)
-print(test3)
+        test3= (height/3)
+        print(test3)
 
-test4= (1+2*5)
-print(test4)
+        test4= (1+2*5)
+        print(test4)
+    elif opcion == "4":
+    #Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+        celsius= int(input("Enter the temperature in Celsius:"))
+        fahren = (celsius *9/5)+32
+        print(fahren)
 
-#Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
 
-celsius= int(input("Enter the temperature in Celsius:"))
-fahren = (celsius *9/5)+32
-print(fahren)
+
+    else:
+        break
+        print("amo")
+
+
+
+
+
 
 
---
+++ menu.py
@@ -0,0 +1,16 @@
+while True:
+    print(" PAGINA PRINCIPAL \n1. Opcion 1\n2. Opcion 2\n3. opcion 3\n4. opcion 4\nSeleccione una opcion")
+    opcion = input(">> ")
+
+    if opcion == "1":
+        # opcion 1
+        print("hi")
+    elif opcion == "2":  # 
+        # opcion 2
+        print("gaby")
+    elif opcion == "3":
+        # opcion 3
+        print("la")
+    else:
+        break
+        print("amo")
