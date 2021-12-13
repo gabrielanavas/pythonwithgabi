9f1c90a9ba2b9874a5c5bb62fecde42ab2786dd0
gabrielanavas <navasgabriela17@gmail.com>
2021-12-13 07:45:03 -0600

    mistakes

--- datastructure_week4.py
+++ datastructure_week4.py
@@ -1,3 +1,30 @@
 #Programming
 #Algorithms: A set of rules or steps used to solve a problem
 #Data Structures: a particular way of organizainf data in a computer
+
+
+
+#lists are surroundend by square brackets, can be any python objects
+friends= ["Sarah", "Jenn", "Lorri", "Andrea", " Astrid"]
+print(friends[1])
+
+#lists a re mutable, meaning we can change 
+
+#The range function returns a list of numbers that range from zero to one less than the parameter
+
+print(range(4))
+
+friends= ["Sarah", "Jenn", "Lorri", "Andrea", " Astrid"]
+print(len(friends))
+print(range(len(friends)))
+
+#concatenating list using +
+a= [1,2,3]-
+b=[4,5,6]
+c= a+b
+print(c)
+
+#Slicing lists
+t= [9,41,12,3,74,15]
+t[1:3]
+
