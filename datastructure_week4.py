0de7558b484a7a62b993466567aaa3b8e70f4f4e
gabrielanavas <navasgabriela17@gmail.com>
2021-12-26 18:58:02 -0600

    sends

--- datastructure_week4.py
+++ datastructure_week4.py
@@ -28,3 +28,54 @@ print(c)
 t= [9,41,12,3,74,15]
 t[1:3]
 
+
+#building lists from scratch 
+#The list stays in order and new elements are added at the end of the list
+stuff= list()
+stuff.append("BOOK")
+stuff.append("99")
+print(stuff)
+stuff.append("cookie")
+print(stuff)
+
+#Something in a list 
+some =[1,9,21,10,16]
+
+print (9 in some)
+print(4 in some)
+print (13 not in some)
+
+# Lista are in order 
+friends =["Jenn", "Sarah", "Mayra", "Johnnie", "Mary", "Andrea"]
+friends.sort()
+print(friends)
+
+
+nums= [3,41,25,5,21]
+print(len(nums))
+print(max(nums))
+print(min(nums))
+print(sum(nums))
+print(sum(nums)/len(nums))
+
+#strings and lists
+abc= "With three words"
+stuff= abc.split()
+print(stuff)
+
+
+#The double split pattern
+word =("From stephen.marquad@uct.ac.za Sat Jan 5 09:14:16 2008")
+words= word.split()
+print(words)
+email= words[1]
+print(email)
+pieces = email.split("@")
+print(pieces)
+
+x = list(range(5))
+print(x)
+
+fruit = 'Banana'
+fruit[0] = 'b'
+print(fruit)
--- send.py
+++ send.py
@@ -2,19 +2,15 @@
 import pywhatkit as kit
 
 names =["estimado emprendedor","estimado emprendedor","estimado emprendedor", "estimado emprendedor","estimado emprendedor","estimado emprendedor","estimado emprendedor","estimado emprendedor","estimado emprendedor","estimado emprendedor","estimado emprendedor","estimado emprendedor"]
-phones = ["+50487999925",
-"+50497672273",
-"+50495010001",
-"+50432443451"]
+phones = ["+50497267596","+50494749475","+50496364978","+50499757293","+5049972-9241"]
 
 
 texts = []
 for name in names:
-    texts.append("Hola {fname}!  Le saluda Gabriela N. de Impact Hub Tegucigalpa.\n Recordarle que está pendiente la agenda de la reunión del Programa \n Favor si puede agendarla el día de hoy a través de este link: https://calendly.com/innovaccion/levantamiento-de-linea-base".format(fname=name))
+    texts.append("Hola {fname}!  Le saluda Gabriela N. de Impact Hub Tegucigalpa.\n  Es de nuestro agrado poder compartir contigo el programa Propulsión de Impacto. \n El programa está compuesto por cinco componentes: desarrollo de capacidades, acompañamiento, acceso a recursos, conexiones y visibilización con el objetivo de potenciar el crecimiento y ampliar las oportunidades de negocios de 100 MiPymes en Honduras. \n Por lo cual nos gustaría saber ¿Cuantas son sus ventas ANUALES para los siguientes años?\n \n  Total ventas anuales 2019:\n Total ventas anuales 2020:\n Total ventas anuales 2021:".format(fname=name))
 
-
-hour = 15
-minute = 33
+hour = 9
+minute = 50
 for i, phone in enumerate(phones):
     kit.sendwhatmsg(phone, texts[i], hour, minute)
     minute = minute + 2
