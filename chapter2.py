f82aa1314ab645ca6fbf14a63128ac7f73e2daac
gabrielanavas <navasgabriela17@gmail.com>
2021-12-27 23:05:55 -0600

    try

--- chapter2.py
+++ chapter2.py
@@ -4,7 +4,44 @@ while True:
 
     if opcion == "1":
     #Exercise 1
-        name = input("What is your name?")
+   #Exercise 2
+name = input("What is your name?")
+print("Hello", name)
+
+#Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay
+hour =input("Enter Hours:")
+rate =input("Enter rate:")
+
+hours= float(hour)
+rates=float(rate)
+
+pay = (hours*rates)
+print(pay)
+
+#Exercise 4 write the value and type of the expression 
+width =17
+height = 12.0
+
+test1= (width//2)
+print(test1)
+
+test2 =(width/2.0)
+print(test2)
+
+test3= (height/3)
+print(test3)
+
+test4= (1+2*5)
+print(test4)
+
+#Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+
+celsius= int(input("Enter the temperature in Celsius:"))
+fahren = (celsius *9/5)+32
+print(fahren)
+
+
+     name = input("What is your name?")
         print("Hello", name)
     elif opcion == "2":  
     #Exercise 2: Write a program to prompt the user for hours and rate per hour to compute gross pay
