72e6895cac86945dd174df74973b47bc8fa6286b
gabrielanavas <navasgabriela17@gmail.com>
2022-01-04 14:19:03 -0600

    changes

--- send.py
+++ send.py
@@ -1,17 +1,20 @@
 # prueba aja
 import pywhatkit as kit
 
-names =["estimado emprendedor","estimado emprendedor","estimado emprendedor", "estimado emprendedor","estimado emprendedor","estimado emprendedor","estimado emprendedor","estimado emprendedor","estimado emprendedor","estimado emprendedor","estimado emprendedor","estimado emprendedor"]
-phones = ["+50497267596","+50494749475","+50496364978","+50499757293","+5049972-9241"]
+names ="estimado emprendedor"
+numbers= ["96738587"]
 
 
 texts = []
 for name in names:
-    texts.append("Hola {fname}!  Le saluda Gabriela N. de Impact Hub Tegucigalpa.\n  Es de nuestro agrado poder compartir contigo el programa Propulsión de Impacto. \n El programa está compuesto por cinco componentes: desarrollo de capacidades, acompañamiento, acceso a recursos, conexiones y visibilización con el objetivo de potenciar el crecimiento y ampliar las oportunidades de negocios de 100 MiPymes en Honduras. \n Por lo cual nos gustaría saber ¿Cuantas son sus ventas ANUALES para los siguientes años?\n \n  Total ventas anuales 2019:\n Total ventas anuales 2020:\n Total ventas anuales 2021:".format(fname=name))
+    texts.append('''Hola {fname}!  Le saluda Gabriela N. de Impact Hub Tegucigalpa.\n Es de nuestro agrado poder compartir contigo el programa Propulsión de Impacto. 
+\nPropulsión de Impacto  es un programa de incubación de 10 semanas y 12 meses de acompañamiento facilitado por  Impact Hub Tegucigalpa, con el apoyo del Proyecto Transformando Sistemas de Mercado (TMS) de USAID, y aliados 
+\nEl programa está compuesto por cinco componentes: desarrollo de capacidades, acompañamiento, acceso a recursos, conexiones y visibilización con el objetivo de potenciar el crecimiento y ampliar las oportunidades de negocios de 100 MiPymes en Honduras. 
+\nEl siguiente paso en el proceso es la firma de un acuerdo de compromiso, que podrás descargar adjunto a el correo electronico o me lo puedes solicitar y deberás enviarnos firmado a más tardar el día 4 de  Enero del 2022. Tenga en cuenta que el comienzo del programa sera en Enero.'''.format(fname=name))
 
-hour = 9
-minute = 50
-for i, phone in enumerate(phones):
-    kit.sendwhatmsg(phone, texts[i], hour, minute)
+hour = 4
+minute = 37
+for i, number in enumerate(corrected):
+    kit.sendwhatmsg(corrected, texts[i], hour, minute)
     minute = minute + 2
 
